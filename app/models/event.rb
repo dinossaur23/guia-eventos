0045929a02fd476883930e80524c6234b80757e4
Raquel <raquel.queiroz@youse.com.br>
2018-05-25 23:41:49 -0300

    add image to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   def self.search_events(param)
     Event.where('name like ? OR zip_code like ?', "%#{param}%", "%#{param}%")
   end
